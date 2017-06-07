688636fd6c90c37fec964226400f29571c9c8948
DusanSacha <sacha.dusan@gmail.com>
2017-06-07 14:40:10 +0200

    server change

--- src/server/main.js
+++ src/server/main.js
@@ -6,10 +6,10 @@ import  { MongoClient } from 'mongodb';
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join('/dist')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/dist/index.html');
+  response.sendFile('/dist/index.html');
 });
 
 let db;
